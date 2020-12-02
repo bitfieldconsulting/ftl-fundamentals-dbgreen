7602cfea5b673f4c15fb5ad5d1d1a2c6adc8bc9a
Bryan Green <bgreen@firstorion.com>
2020-12-02 08:29:51 -0600

    Revert "Merge branch 'my_first_work'"
    This reverts commit 9b7af9c6a295d94826875832a56e5eb2de008c27, reversing
    changes made to 6a77ef9663bc00263c9e9c17bb627774fc2ac850.

--- calculator.go
+++ calculator.go
@@ -1,28 +1,13 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
-import "errors"
-
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
-	return a + b
+return a + b
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
-	return a - b
-}
-
-// Multiply takes two numbers and returns the result of multiplying them together
-func Multiply(a, b float64) float64 {
-	return a * b
-}
-
-// Divide takes two numbers and returns the result of dividing a by b
-func Divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, errors.New("Divide by Zero")
-	}
-	return a / b, nil
+	return b - a
 }
--- calculator_test.go
+++ calculator_test.go
@@ -7,83 +7,18 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	type testCase struct {
-		a, b float64
-		want float64
-	}
-	testCases := []testCase{
-		{a: 2, b: 2, want: 4},
-		{a: 1, b: 1, want: 2},
-		{a: 5, b: 0, want: 5},
-	}
-	for _, tc := range testCases {
-		got := calculator.Add(tc.a, tc.b)
-		if tc.want != got {
-			t.Errorf("Add (%f,%f): want %f, got %f", tc.a, tc.b, tc.want, got)
-		}
+	var want float64 = 4
+	got := calculator.Add(2, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
 	}
 }
 
-func TestSubtract(t *testing.T) {
-	t.Parallel()
-	type testCase struct {
-		a, b float64
-		want float64
-	}
-	testCases := []testCase{
-		{a: 1, b: 2, want: -1},
-		{a: 100, b: 99, want: 1},
-		{a: 3, b: 3, want: 0},
-	}
-	for _, tc := range testCases {
-		got := calculator.Subtract(tc.a, tc.b)
-		if tc.want != got {
-			t.Errorf("Subtract(%f,%f): want %f, got %f", tc.a, tc.b, tc.want, got)
-		}
-	}
-}
-func TestMultiply(t *testing.T) {
-	t.Parallel()
-	type testCase struct {
-		a, b float64
-		want float64
-	}
-	testCases := []testCase{
-		{a: -1, b: 5, want: -5},
-		{a: 9, b: 3, want: 27},
-		{a: 5, b: 5, want: 25},
-	}
-	for _, tc := range testCases {
-		got := calculator.Multiply(tc.a, tc.b)
-		if tc.want != got {
-			t.Errorf("Multiply(%f,%f): want %f, got %f", tc.a, tc.b, tc.want, got)
-		}
-	}
-}
-func TestDivide(t *testing.T) {
-	t.Parallel()
-	type testCase struct {
-		a, b        float64
-		want        float64
-		errExpected bool
-	}
-	testCases := []testCase{
-		{a: 6, b: 0, want: 0, errExpected: true},
-		{a: 6, b: 3, want: 2, errExpected: false},
-		{a: 10, b: 4, want: 2.5, errExpected: false},
-	}
-
-	for _, tc := range testCases {
-		got, e := calculator.Divide(tc.a, tc.b)
-		if tc.errExpected == true && e == nil {
-			t.Errorf("Divide (%f,%f): %s", tc.a, tc.b, e)
-		} else if tc.errExpected == false && e != nil {
-			t.Errorf("Divide(%f,%f): Unexpected Error", tc.a, tc.b)
-		}
-
-		if !tc.errExpected && tc.want != got {
-			t.Errorf("Divide (%f,%f): wanted %f, got %f", tc.a, tc.b, tc.want, got)
-		}
-
-	}
-}
+// func TestSubtract(t *testing.T) {
+// 	t.Parallel()
+// 	var want float64 = 2
+// 	got := calculator.Subtract(4, 2)
+// 	if want != got {
+// 		t.Errorf("want %f, got %f", want, got)
+// 	}
+// }
